d72f3a8193b57bf34861810d159a5498180eac56
Webappmaster-Eugene <johnn.hotmail@mail.ru>
2024-06-01 12:11:28 +0300

    fix: context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,38 @@
-import type { Metadata } from 'next';
+"use client";
 import { Open_Sans } from 'next/font/google';
 import './globals.css';
 import styles from './page.module.css';
 import Github from '../public/Github.svg';
 import Link from 'next/link';
-import { GitHubProvider, GitHubContext } from '@/context/GitHubContext';
-import React, { useContext, ReactNode } from 'react';
+import {GitHubContext} from '@/app/context/GitHubContext';
+import React, {useContext, ReactNode, ContextType} from 'react';
 
 const openSans = Open_Sans({ subsets: ['latin', 'cyrillic'] });
-
-export const metadata: Metadata = {
-	title: 'Blog',
-	description: 'Blog about JP'
-};
 
 export default function RootLayout({
 	children
 }: Readonly<{
 	children: ReactNode;
 }>) {
-	const { gitHubUrl } = useContext(GitHubContext)!;
+
+	const mycontext = useContext(GitHubContext);
 
 	return (
 		<html lang="ru">
-			<GitHubProvider>
 				<body className={openSans.className}>
+				<GitHubContext.Provider value={'https://github.com'}>
 					<div className={styles.header}>
 						<Link className={styles.logo} href={'/'}>
 							.my_blog
 						</Link>
-						<Link href={gitHubUrl} target="_blank">
+						<Link href={mycontext} target="_blank">
 							<Github />
 						</Link>
 					</div>
 					{children}
+				</GitHubContext.Provider>
 				</body>
-			</GitHubProvider>
+
 		</html>
 	);
 }
